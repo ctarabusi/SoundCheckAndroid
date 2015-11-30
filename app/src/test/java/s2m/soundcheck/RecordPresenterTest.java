fd67613922d9081377fd0f72880c7eeb0b9e1fa9
Christian Tarabusi <cta@sinnerschrader-mobile.com>
2015-11-30 17:18:58 +0100

    Update package and removed obsolete tests

---
+++ app/src/test/java/s2m/soundcheck/RecordPresenterTest.java
@@ -0,0 +1,26 @@
+package s2m.soundcheck;
+
+import org.junit.Test;
+
+import s2m.soundcheck.firstusecase.interactor.CommentsInteractor;
+import s2m.soundcheck.firstusecase.view.CommentsPresenter;
+import s2m.soundcheck.recording.view.RecordPresenter;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by cta on 16/09/15.
+ */
+public class RecordPresenterTest
+{
+    @Test
+    public void testPresenterOnCreate()
+    {
+
+        RecordPresenter presenter = new RecordPresenter();
+
+        // TO ADD TESTS
+    }
+
+}
